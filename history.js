fd4406ce5c98f290668e0bcf6881251ebadb6917
Sabid Hasan <sabid.soft@gmail.com>
2022-09-15 22:39:20 +0600

    solved seconds problem

--- history.js
+++ history.js
@@ -1,6 +1,7 @@
 const histories = document.getElementById("histories");
 
 function addHistory(questionText, timeTaken, errorCount) {
+  timeTaken = parseInt(timeTaken)
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
